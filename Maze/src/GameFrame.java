e6de5840811d193f83ae6feeeaaf7e26c56b6d38
星 優太 <youtaixing753@gmail.com>
2024-07-09 11:42:22 +0900

    スペース以外できました。スペースは今作業しています

--- Maze/src/GameFrame.java
+++ Maze/src/GameFrame.java
@@ -1,17 +1,10 @@
 
-public class GameFrame extends MyFrame{
-	public void run() {
-		Wall wall1=new Wall(10,50,3,0);
-		for(int j=0; j<=3; j++) {
-		for(int i=0; i<=15; i++) {
-		
-		wall1.draw(this);
-		wall1.move();
-		
-		}
-		
-		}
-	
-	}
 
+public class GameFrame {
+		public static void main(String[] args){
+			new MyFrame();
+	    }
+		static boolean spacePressed; 
+		 
 }
+
